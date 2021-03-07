9686627c98e8c23a4d36d511f011d6496c53147c
Alexandr Kaliberda <sashakaliberda@gmail.com>
2021-03-07 18:02:30 +0300

    init

---
+++ cmd/app/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+const configPath = "configs/main"
+
+func main() {
+	fmt.Println("Hello!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alex-dev-master/fibonacci.git
+
+go 1.16
